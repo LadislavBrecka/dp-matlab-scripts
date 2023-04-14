3515a8c980a612d08d3af6f32e52341a0eb4c875
LadislavBrecka <lacoobrecka@gmail.com>
2023-04-14 20:32:55 +0200

    Add AW Gain experiments, tune sim in pos_piv.m

--- chap4/pos_regulation/pos_piv.m
+++ chap4/pos_regulation/pos_piv.m
@@ -1,3 +1,23 @@
+%ideal signal parameters
+% 
+% u_speed_real = u_speed_real(1, 1515:1515+4000);
+% y_speed_real = y_speed_real(1, 1515:1515+4000);
+% w_pos_real = w_pos_real(1, 1515:1515+4000);
+% y_pos_real = y_pos_real(1, 1515:1515+4000);
+%
+% Ts = 0.01;
+% b = 0.95;
+% w = 4.2;
+% k = 1.8;
+% P = 0.99212598425196863;
+% I = 3.78223425915421;
+% V = 0.83758762153302224;
+% AW = 2.7;
+% u_min = -100;
+% u_max = 100;
+% G_S = tf([0, 0.08348760837171855], [1, -0.97343313690447564], Ts, 'Variable', 'z^-1');
+% Int = tf([Ts, Ts], [2.0, -2.0], Ts, 'Variable', 'z^-1');
+
 
 %% READ EXPERIMENT SIGNALS
 clc, clear all
@@ -11,36 +31,35 @@ y_speed_real = table2array(T(3, :));
 w_pos_real = table2array(T(4, :));
 y_pos_real = table2array(T(5, :));
 
-u_speed_real = u_speed_real(1, 1354:1354+4000);
-y_speed_real = y_speed_real(1, 1354:1354+4000);
-w_pos_real = w_pos_real(1, 1354:1354+4000);
-y_pos_real = y_pos_real(1, 1354:1354+4000);
+u_speed_real = u_speed_real(1, 1515:1515+4000);
+y_speed_real = y_speed_real(1, 1515:1515+4000);
+w_pos_real = w_pos_real(1, 1515:1515+4000);
+y_pos_real = y_pos_real(1, 1515:1515+4000);
     
 % plot(y_pos_real); hold on;
 % plot(w_pos_real); hold on;
 %% SPECIFY EXPERIMENTAL DATA FOR SIMULATION
-
-% simulation data specification
 Ts = 0.01;
-b = 0.9;
-w = 4;
-k = 2.0;
-P = 1.0526315789473684;
-I = 3.5926734620137482;
-V = 0.76904319363933893;
-AW = 5;
 u_min = -100;
 u_max = 100;
-G_S = tf([0, 0.08348760837171855], [1, -0.97343313690447564], Ts, 'Variable', 'z^-1');
-Int = tf([Ts, Ts], [2.0, -2.0], Ts, 'Variable', 'z^-1');
 
-% create referent tf
+G_S = tf([0, 0.08348760837171855], [1, -0.97343313690447564], Ts, 'Variable', 'z^-1');
 A = cell2mat(G_S.denominator);
 B = cell2mat(G_S.numerator);
-gain =  sum(B) / sum(A);
+Int = tf([Ts, Ts], [2.0, -2.0], Ts, 'Variable', 'z^-1');
+
+w = 4.2;
+b = 0.95;
+k = 1.8;
+AW = 2.7;
+
+P = 0.99212598425196863;
+I = 3.78223425915421;
+V = 0.83758762153302224;
+
+% create referent tf
 G_ref_s = tf((w^2 * k), [1, 2*b*w + k, w^2 + 2*b*w*k, w^2 * k]);
 G_ref_z = c2d(G_ref_s, Ts, 'matched');
-
 
 % create input signal for simulation - same as experiment signal
 t_step_sim = 0:0.01:40;
@@ -48,14 +67,14 @@ w_pos_sim = timeseries(w_pos_real, t_step_sim);
 
 %% RUN SIMULATION
 
-% run reference model
+%run reference model
 [y_pos_ref, t_ref] = lsim(G_ref_z, w_pos_real);
 
-% run simulation
+%run simulation
 out = sim('piv_simulation.slx');
 y_pos_matlab = out.y_matlab.data(1: 4001);
 
-%% PLOTTING
+% PLOTTING
 
 figure(1)
 plot(t_step_sim, w_pos_real, 'DisplayName', 'SP'); 
@@ -77,13 +96,16 @@ ylabel('PWM stride [ % ]')
 xlabel('Time [s]')
 title('Experiment control variable U')
 
-%% MSE Computation
+% MSE Computation
 
 mse = 0;
 e = zeros(1, length(y_pos_matlab));
 for i=1:length(y_pos_matlab)
     m = y_pos_matlab(i); n = y_pos_real(i);
     e(i) = m - n;
+%     if abs(e(i)) <= 2
+%         e(i) = 0;
+%     end
 
     mse = mse + (e(i))^2;
 end
@@ -94,6 +116,6 @@ plot(t_step_sim, e, 'DisplayName', 'Difference: Matlab - C++')
 legend
 xlabel('Sample [k]')
 ylabel('-')
-% ylim([-0.005, 0.005])
+ylim([-0.005, 0.005])
 title(strcat('MSE = ', num2str(mse)))
 grid on
